84ae475f9e864c0c37c12e8318cb87fb1986bc17
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2022-09-28 15:03:40 +0800

    Update linear_gradient.py

--- homeworks/linear_gradient.py
+++ homeworks/linear_gradient.py
@@ -13,9 +13,9 @@ color2 = [0, 128, 255]
 
 for i, v in enumerate(np.linspace(0, 1, image.shape[0])):
     for j, v2 in enumerate(np.linspace(0, 1, image.shape[1])):
-        r = lerp(color1[0], color2[0], (v + v2) / 2)
+        r = lerp(color1[2], color2[2], (v + v2) / 2)
         g = lerp(color1[1], color2[1], (v + v2) / 2)
-        b = lerp(color1[2], color2[2], (v + v2) / 2)
+        b = lerp(color1[0], color2[0], (v + v2) / 2)
         image[i, j, :] = [r, g, b]
 
 plt.figure(1)
