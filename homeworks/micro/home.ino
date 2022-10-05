d565d2fc5416b44d603de3dc5e860f60366346d0
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2022-10-05 11:03:17 +0800

    Create home.ino

---
+++ homeworks/micro/home.ino
@@ -0,0 +1,20 @@
+#define sensor_pin A2
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  int val = analogRead(sensor_pin);
+  Serial.println(val);
+  delay(10);
+  if (val < 850){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+  }
+  else{
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+  }
+}
