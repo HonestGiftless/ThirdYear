de7de817710b1df03663f1bbf49aa0be1a366b03
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2022-11-13 20:00:52 +0800

    Delete main.kt

--- homeworks/Design and development of applications for mobile devices/html/main.kt
+++
@@ -1,6 +0,0 @@
-<html> 
-  <table> 
-    <style> html{background-color:#FFFAFA; font-size: 100%; font-family: Roboto; color: #008080;}</style> 
-    <tr><td>address</td><td>askbill@microsoft.com</td></tr> 
-  </table> 
-</html> 
