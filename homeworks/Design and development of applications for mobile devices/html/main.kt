ec93f36bca3b8340474a27f1d860108a15c4df63
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2022-11-13 19:55:48 +0800

    Update main.kt

--- homeworks/Design and development of applications for mobile devices/html/main.kt
+++ homeworks/Design and development of applications for mobile devices/html/main.kt
@@ -6,10 +6,10 @@ data class Message(val address: String?, val topic: String?) {
 
 fun main() {
     fun Message.toHTML(): String {
-        var template = "<html> \n<table> \n<style> html{background-color:#FFFAFA; font-size: 100%; font-family: Roboto; color: #008080;}</style> \n"
-        address?.let {template += "<tr><td>address</td><td>$it</td></tr> \n" }
-        topic?.let { template +="<tr><td>address</td><td>$it</td></tr> \n" }
-        template += "</table> \n</html> \n"
+        var template = "<html> \n  <table> \n    <style> html{background-color:#FFFAFA; font-size: 100%; font-family: Roboto; color: #008080;}</style> \n"
+        address?.let {template += "    <tr><td>address</td><td>$it</td></tr> \n" }
+        topic?.let { template +="    <tr><td>address</td><td>$it</td></tr> \n" }
+        template += "  </table> \n</html> \n"
         return template
     }
 
