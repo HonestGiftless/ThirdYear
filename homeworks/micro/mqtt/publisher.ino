85248d7517d2ed962454fad2bca7a538d38d0e04
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2022-12-21 10:36:09 +0800

    Update publisher.ino

--- homeworks/micro/mqtt/publisher.ino
+++ homeworks/micro/mqtt/publisher.ino
@@ -5,19 +5,8 @@ void setup()
   Serial.begin(9600);
 }
 
-uint16_t value = 0;
-uint8_t data[2];
-
 void loop()
 {
-  if (Serial.available())
-  {
-    if (Serial.read())
-    {
-      int16_t value = analogRead(photo_pin);
-      data[0] = value >> 8 & 0xFF;
-      data[1] = value & 0xFF;
-      Serial.write(data, 2);
-    }
-  }
+  int val_l = analogRead(photo_pin);
+  Serial.print(val_l);
 }
