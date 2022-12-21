96741d86e988e2133c56eea71128c40461d3eb03
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2022-12-21 10:51:00 +0800

    Update publisher.ino

--- homeworks/micro/mqtt/publisher.ino
+++ homeworks/micro/mqtt/publisher.ino
@@ -1,12 +1,25 @@
-#define photo_pin A0
-
-void setup()
-{
-  Serial.begin(9600);
-}
-
-void loop()
-{
-  int val_l = analogRead(photo_pin);
-  Serial.print(val_l);
+#define sensor_pin A0
+void setup() { 
+  Serial.begin(9600); 
+} 
+ 
+int status = 0; 
+unsigned long last_millis = 0; 
+unsigned long interval = 1000; 
+ 
+void loop() { 
+  if (Serial.available()>0 || status == 1){ 
+    char message = Serial.read();
+    if (message == 's' || status == 1){ 
+      if (millis() - last_millis > interval){ 
+        last_millis = millis(); 
+        int val = analogRead(sensor_pin); 
+        Serial.println(val); 
+        status = 1; 
+      } 
+    } 
+    else{ 
+      Serial.println("Unknown message"); 
+    } 
+  } 
 }
