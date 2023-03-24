78f8b07cb1d7edebcc7e19f0ec1372372e730e6f
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2023-03-24 15:10:02 +0800

    Create send_message_to_arduino.py

---
+++ sixth_semester/microcontrollers/wifi-esp-arduino-car/send_message_to_arduino.py
@@ -0,0 +1,23 @@
+from machine import UART
+import uos
+
+class Go:
+  def __init__(self):
+    self.uart = UART(0, 115200)
+    self.uart.init(115200, bits=8, parity=None, stop=1)
+    uos.dupterm(None, 1)
+
+  def forward(self):
+    self.uart.write('f')
+  
+  def left(self):
+    self.uart.write('l')
+  
+  def right(self):
+    self.uart.write('r')
+
+  def stop(self):
+    self.uart.write('s')
+
+go = Go()
+# go.forward() - пример команды для отправки
