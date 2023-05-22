ce5f92f62ec7438f43fd20550233975e899954c7
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2023-05-22 13:15:26 +0300

    Add files via upload

---
+++ sixth_semester/microcontrollers/Project/ind_and_sonic.ino
@@ -0,0 +1,58 @@
+#define trigPin 12
+#define echoPin 11
+
+const int indPin = 2;
+long duration, cm;
+
+bool item = false;
+bool hasMetall = false;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(indPin, INPUT);
+}
+ 
+void loop() {
+  takeObject();
+  checkMetall(item);
+  if (item && hasMetall) {
+    Serial.print('YEEEEP');
+  }
+}
+
+void takeObject() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  cm = (duration / 2) / 29.1;
+
+  delay(250);
+
+  if (cm < 140) {
+    item = true;
+  } else {
+    item = false;
+  }
+}
+
+void checkMetall(bool itm) {
+  int sensorValue = digitalRead(indPin);
+
+  if (itm) {
+    if (sensorValue==LOW) {
+      hasMetall = true;
+    } else {
+      hasMetall = false;
+    }
+  } else {
+    hasMetall = false;
+  }
+}
